9726fe3f8e378c9dee0b92ea83c54c3a948a02eb
John Riccardi <riccjohn@gmail.com>
2018-09-19 17:32:47 -0500

    Got test toggle switch working

--- src/components/AnimeTest.js
+++ src/components/AnimeTest.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import anime from 'animejs';
 
 class AnimeTest extends Component {
-  componentDidMount() {
-    var cssSelector = anime({
-      targets: '#cssSelector',
-      duration: 3000,
-      translateX: 250,
+  constructor() {
+    super();
+    this.state = {
+      on: false,
+    };
+    this.slide = this.slide.bind(this);
+  }
+  slide() {
+    anime({
+      targets: '.slider',
+      duration: 250,
+      translateX: this.state.on ? 0 : 47,
+      easing: [0.4, 0.1, 0.6, 0.9],
+      elasticity: 0,
+    });
+
+    this.setState({
+      on: !this.state.on,
     });
   }
+
+  componentDidMount() {}
 
   render() {
     return (
       <div className="main-container flex column center-all">
-        <div className="switch flex">
+        {this.state.on ? <h1>ON</h1> : <h1>OFF</h1>}
+        <div className="switch flex" onClick={this.slide}>
           <div className="slider" />
         </div>
       </div>
